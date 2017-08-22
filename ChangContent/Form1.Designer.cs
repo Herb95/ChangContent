5f40e8eac9fc7d76b9f8770c210f472b6d0a59aa
Herbie <1406912774@qq.com>
2017-08-22 18:01:17 +0800

    可以读取文件了

--- ChangContent/Form1.Designer.cs
+++ ChangContent/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container ();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn1
+            // 
+            this.Btn1.Location = new System.Drawing.Point(32, 112);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(91, 39);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.Text = "切换中/英文";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(335, 219);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Btn1);
+            this.Name = "Form1";
+            this.Text = "切换中英文";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ChangContent/Form1.cs
+++ ChangContent/Form1.cs
@@ -16,5 +16,40 @@ namespace ChangContent
         {
             InitializeComponent ();
         }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            ReadTextFile read = new ReadTextFile ();
+            read.ReadFile ();
+        }
+    }
+    class ReadTextFile
+    {
+        public void ReadFile()
+        {
+            //直接读取出字符串
+            //string text = System.IO.File.ReadAllText (@"..\test.txt");
+            string text = System.IO.File.ReadAllText (@"D:\_VisualStudioProject\test\test.txt");
+            Console.WriteLine (text);
+
+            ////按行读取为字符串数组
+            //string[] lines = System.IO.File.ReadAllLines (@"d:\_visualstudioproject\test\test.txt");
+            //foreach (string line in lines)
+            //{
+            //    Console.WriteLine (line.ToString ());
+            //}
+
+            ////从头到尾以流的方式读出文本文件
+            ////该方法会一行一行的读出文件
+            //using (System.IO.StreamReader sr = new System.IO.StreamReader (@"D:\_VisualStudioProject\test\test.txt"))
+            //{
+            //    string str;
+            //    while ((str = sr.ReadLine ()) != null)
+            //    {
+            //        Console.WriteLine (str.ToString());
+            //    }
+            //    Console.Read ();
+            //}
+        }
     }
 }
