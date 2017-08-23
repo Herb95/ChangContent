d8c5f1711558cf2ccb926b8295491ef8e18807e9
Herbie <1406912774@qq.com>
2017-08-23 15:37:50 +0800

    可以读取文件，显示text内容

--- ChangContent/Form1.Designer.cs
+++ ChangContent/Form1.Designer.cs
@@ -32,38 +32,55 @@ namespace ChangContent
         {
             this.Btn1 = new System.Windows.Forms.Button();
             this.Btn2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Btn1
             // 
-            this.Btn1.Location = new System.Drawing.Point(32, 112);
+            this.Btn1.Location = new System.Drawing.Point(12, 12);
             this.Btn1.Name = "Btn1";
             this.Btn1.Size = new System.Drawing.Size(91, 39);
             this.Btn1.TabIndex = 0;
-            this.Btn1.Text = "切换中文";
+            this.Btn1.Text = "读取文件";
             this.Btn1.UseVisualStyleBackColor = true;
             this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
             // 
-            // button2
+            // Btn2
             // 
-            this.Btn2.Location = new System.Drawing.Point(181, 112);
+            this.Btn2.Location = new System.Drawing.Point(109, 12);
             this.Btn2.Name = "Btn2";
             this.Btn2.Size = new System.Drawing.Size(91, 39);
             this.Btn2.TabIndex = 1;
-            this.Btn2.Text = "切换英文";
+            this.Btn2.Text = "修改文件";
             this.Btn2.UseVisualStyleBackColor = true;
             this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(188, 217);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 219);
+            this.ClientSize = new System.Drawing.Size(484, 316);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Btn2);
             this.Controls.Add(this.Btn1);
             this.Name = "Form1";
-            this.Text = "切换中英文";
+            this.Text = "文本查询";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +89,7 @@ namespace ChangContent
 
         private System.Windows.Forms.Button Btn1;
         private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ChangContent/Form1.cs
+++ ChangContent/Form1.cs
@@ -16,15 +16,21 @@ namespace ChangContent
         {
             InitializeComponent ();
         }
+        ReadTextFile read = new ReadTextFile ();
 
         private void Btn1_Click(object sender, EventArgs e)
         {
-            ReadTextFile read = new ReadTextFile ();
-            read.ReadFile ();
+            List<string> s = read.ReadFile1 ();
+            for (int i = 0; i < s.Count; i++)
+            {
+                textBox1.Text += s[i];
+            }
         }
         private void Btn2_Click(object sender, EventArgs e)
         {
-           
+            textBox1.Text = "";
+            WriteTextFile write = new ChangContent.WriteTextFile ();
+            write.WriteFile ();
         }
     }
     class ReadTextFile
@@ -43,17 +49,57 @@ namespace ChangContent
             //    Console.WriteLine (line.ToString ());
             //}
 
-            ////从头到尾以流的方式读出文本文件
-            ////该方法会一行一行的读出文件
-            //using (System.IO.StreamReader sr = new System.IO.StreamReader (@"D:\_VisualStudioProject\test\test.txt"))
-            //{
-            //    string str;
-            //    while ((str = sr.ReadLine ()) != null)
-            //    {
-            //        Console.WriteLine (str.ToString());
-            //    }
-            //    Console.Read ();
-            //}
+         
+        }
+        public List<string> ReadFile1()
+        {
+            //从头到尾以流的方式读出文本文件
+            //该方法会一行一行的读出文件
+            List<string> a = new List<string> ();
+            using (System.IO.StreamReader sr = new System.IO.StreamReader (@"D:\_VisualStudioProject\test\test.txt"))
+            {
+                string str;
+                while ((str = sr.ReadLine ()) != null)
+                {
+                    a.Add (str.ToString ());
+                    //Console.WriteLine (str.ToString ());
+                }
+                Console.Read ();
+                return a;
+            }
+        }
+    }
+
+    class WriteTextFile
+    {
+        public void WriteFile()
+        {
+            //如果文件不存在，则创建；存在则覆盖
+            //该方法写入字符数组换行显示
+            string[] lines = { "first line", "second line", "third line", "第四行第四行第四行第四行第四行第四行" };
+            System.IO.File.WriteAllLines (@"D:\_VisualStudioProject\test\test.txt", lines, Encoding.UTF8);
+
+            //如果文件不存在，则创建;存在则覆盖
+            string strTest = "该例子测试一个字符串写入文本文件。";
+            System.IO.File.WriteAllText (@"D:\_VisualStudioProject\test\test.txt", strTest, Encoding.UTF8);
+
+            //在将文本写入文件前，处理文本行
+            //streamWriter一个参数默认覆盖
+            //streamWriter第二个参数为false覆盖现有文件，为true则把文本追加到文本末尾
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter (@"D:\_VisualStudioProject\test\test.txt", true))
+            {
+                foreach (string line in lines)
+                {
+                    if (!line.Contains("second"))
+                    {
+                        //直接追加文件末尾不换行
+                        file.Write (line);
+                        //直接追加文件末尾，换行
+                        file.WriteLine (line);
+                    }
+
+                }
+            }
         }
     }
 }
